aa8d6091f8e12ea73522bb24b4ac1afaf9fa3984
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-04-19 21:01:03 +0700

    minor reformat

--- review_crawl.py
+++ review_crawl.py
@@ -72,8 +72,8 @@ if not NUM_REVIEWS: NUM_REVIEWS = None
 # get links and create output file
 with open(parser['links']['file_location'], encoding='utf-8') as file:
     links = file.read().split('\n')
-if not osp.exists('output/'):
-    Path.mkdir('output')
+
+if not osp.exists('output/'): Path.mkdir('output')
 reviews_file = open('output/reviews.csv', 'w', encoding='utf-8')
 metedata_file = open('output/place_metadata.csv', 'w', encoding='utf-8')
 reviews_file.write('review,rating,place_id\n')
