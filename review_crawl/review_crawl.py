a0efc1b93ba6385d3a6774ca724840f63243350b
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-04-26 13:54:17 +0700

    major code refactoring
    Package code into an object for customization, this includes:
    - Opening a Firefox web driver
    - Crawling reviews and place metadata from a given Google Place and returning the result
    
    This object does not handle how the results are saved to a file to provide users a customizable way of defining this process.

--- review_crawl/review_crawl.py
+++ review_crawl/review_crawl.py
@@ -1,8 +1,5 @@
-import configparser
 import time
-import os.path as osp
 
-from pathlib import Path
 from traceback import format_exc
 
 
@@ -20,99 +17,86 @@ from utils import (
     scroll_reviews
 )
 
+
+from typing import Literal
+
 # logging stuff
 import log_setup # this just sets up logging, it's not meant to be used as a module
 import logging
 logger = logging.getLogger(__name__)
 
 
-def crawl_process(
-) -> tuple[list[str], list[str]] | None:
-    # go to reviews tab by clicking reviews button
-    driver.find_element(By.XPATH, "//button[@class='hh2c6 ' and contains(., 'Bài đánh giá')]")\
-          .click()
-    time.sleep(LOADING_TIMEOUT)
+class NoConnectedDriver(Exception):
+    """Exception raised when there is no initialized WebDriver"""
 
-    print('Begin crawling reviews')
-    logger.info('Loading reviews...')
-    scroll_reviews(driver, NUM_REVIEWS, LOADING_TIMEOUT)
+class ReviewCrawler():
+    _REVIEW_BUTTON = "//button[@class='hh2c6 ' and contains(., 'Bài đánh giá')]"
+    def __init__(self, language: str = 'vi', headless: bool = True):
+        self.driver_opts = FirefoxOptions()
+        self._driver = None
 
-    logger.info('Crawling reviews data...')
-    try:
-        reviews = get_review_texts(driver)
-        ratings = get_review_ratings(driver)
-    except:
-        logger.critical(f"""Exception occured while trying to crawl reviews for {url}
+        self.driver_opts.set_preference("intl.accept_languages", language)
+        if headless:
+            self.driver_opts.add_argument('--headless')
+
+    def open(self):
+        logger.info('Initializing driver...')
+        self._driver = Firefox(options=self.driver_opts)
+        self._wait = WebDriverWait(self._driver, 20)
+        logger.info('Driver initialized...')
+
+    def crawl_from(self,
+        url: str,
+        num_reviews: int,
+        load_timeout: float
+    ) -> dict[Literal['address', 'price_range', 'reviews', 'ratings'], str]:
+        if self._driver is None:
+            raise NoConnectedDriver('No WebDriver have been initialized. Please check if you have opened a connection!')
+        
+        print(f'Now crawling from \033[34;4m{url}\033[0m')
+        logger.info(f'Connecting to {url}')
+        self._driver.get(url)
+
+
+        print('Getting place overview...')
+        meta_data = get_place_meta(self._wait)
+        results = self._crawl_process(url, load_timeout, num_reviews)
+        if results is None:
+            print('\033[31;1mCrawling unsuccesful! Check logs for details...\033[0m')
+        else:
+            print('\033[32;1mCrawling finished!\033[0m')
+        return {
+            'address': meta_data['address'],
+            'price_range': meta_data['price_range'],
+            'reviews': results[0],
+            'ratings': results[1]
+        }
+
+    def close(self,):
+        self._driver.quit()
+
+    
+    def _crawl_process(self,
+        url: str,
+        load_timeout: float,
+        num_reviews: int
+    ) -> tuple[list[str], list[str]] | None:
+        # go to reviews tab by clicking reviews button
+        self._driver\
+            .find_element(By.XPATH, ReviewCrawler._REVIEW_BUTTON)\
+            .click()
+        time.sleep(load_timeout)
+
+        print('Begin crawling reviews')
+        logger.info('Loading reviews...')
+        scroll_reviews(self._driver, num_reviews, load_timeout)
+
+        logger.info('Crawling reviews data...')
+        try:
+            reviews = get_review_texts(self._driver)
+            ratings = get_review_ratings(self._driver)
+        except:
+            logger.critical(f"""Exception occured while trying to crawl reviews for {url}
 {format_exc()}""")
-        return
-    return reviews, ratings
-
-def write_to_file(
-place_id: int,
-reviews: list[str],
-ratings: list[str]):
-    for review, rating in zip(reviews, ratings):
-        review = review.replace('\n', '. ').replace('"', "'")
-        reviews_file.write(f'"{review}",{rating},{place_id}\n')
-
-
-
-
-parser = configparser.ConfigParser()
-print('Getting things ready before crawling...')
-try:
-    parser.read('crawler.conf')
-    LOADING_TIMEOUT = float(parser['crawler_options']['loading_timeout'])
-    HEADLESS = bool(parser['firefox_options']['headless'])
-    NUM_REVIEWS = int(parser['crawler_options']['max_reviews'])
-except Exception as e:
-    logger.critical(f'Could not parse crawler.conf!\n{format_exc(e)}')
-if not NUM_REVIEWS: NUM_REVIEWS = None
-
-# get links and create output file
-output_loc = parser['file_locations']['output_loc']
-with open(parser['file_locations']['input_loc'], encoding='utf-8') as file:
-    links = file.read().split('\n')
-
-if not osp.exists(output_loc): Path.mkdir('output_loc')
-reviews_file = open(osp.join(output_loc, 'reviews.csv'), 'w', encoding='utf-8')
-metedata_file = open(osp.join(output_loc, 'place_metadata.csv'), 'w', encoding='utf-8')
-reviews_file.write('review,rating,place_id\n')
-metedata_file.write('id,url,address,price_range\n')
-
-
-
-options = FirefoxOptions()
-options.set_preference("intl.accept_languages", parser['firefox_options']['language'])
-if HEADLESS: options.add_argument('--headless')
-
-logger.info('Initializing driver...')
-driver = Firefox(options=options)
-wait = WebDriverWait(driver, 10)
-logger.info('Driver initialized...')
-
-
-META_EXC_PLACEHOLDER = {'address': '', 'price_range': ''}
-for i, url in enumerate(links):
-    print(f'Now crawling from \033[34;4m{url}\033[0m')
-    logger.info(f'Connecting to {url}')
-    driver.get(url)
-
-
-    print('Getting place overview...')
-    meta_data = get_place_meta(wait)
-    metedata_file.write(f'{i},"{url}","{meta_data["address"]}",{meta_data["price_range"]}\n')
-
-
-    results = crawl_process()
-    if results is None:
-        print('\033[31;1mCrawling unsuccesful! Check logs for details...\033[0m')
-    else:
-        print('\033[32;1mCrawling finished! Saving to file...')
-        write_to_file(i, *results)
-        print('Saved to file!\033[0m')
-
-
-driver.close()
-metedata_file.close()
-reviews_file.close()
+            return
+        return reviews, ratings
