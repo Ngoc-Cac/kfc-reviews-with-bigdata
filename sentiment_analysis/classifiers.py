b6d63aecb39c49cfceec70199f74043c671919f8
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-04-29 15:44:25 +0700

    modify models
    changed ReviewClassifierWithPhoBERT into BaseClassifierWithPhoBert for easy customization of hidden layers.
    
    add MLPClassifierWithPhoBERT model. This model classifies using pooled output of PhoBERT to feed into two hidden layers of dimension 512 with activation function ReLU

--- sentiment_analysis/classifiers.py
+++ sentiment_analysis/classifiers.py
@@ -2,20 +2,27 @@ from transformers import AutoModel
 from torch import nn
 
 
-class ReviewClassifierWithPhoBERT(nn.Module):
-    def __init__(self):
+class BaseClassifierWithPhoBERT(nn.Module):
+    def __init__(self, hidden_layer):
         super().__init__()
         self.phobert = AutoModel.from_pretrained("vinai/phobert-base-v2")
         # Freeze everything except for the RoBERTa pooling layer
         self.phobert.requires_grad_(False)
         self.phobert.pooler.requires_grad_(True)
         
-        self.mlp = nn.Sequential(
-            nn.Linear(768, 512),
-            nn.ReLU(),
-            nn.Linear(512, 3)
-        )
+        self.hidden_layer = hidden_layer
 
     def forward(self, text_input_ids, attention_mask):
         phobert_embeddings = self.phobert(text_input_ids, attention_mask)
-        return self.mlp(phobert_embeddings['pooler_output'])
+        return self.hidden_layer(phobert_embeddings['pooler_output'])
+    
+class MLPClassifierWithPhoBERT(BaseClassifierWithPhoBERT):
+    def __init__(self):
+        hidden_layer = nn.Sequential(
+            nn.Linear(768, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 3)
+        )
+        super().__init__(hidden_layer)
