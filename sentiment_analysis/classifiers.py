f865b9fc114a32f5fdae0d261be461fe11f7a318
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-04 19:36:00 +0700

    added documentation

--- sentiment_analysis/classifiers.py
+++ sentiment_analysis/classifiers.py
@@ -11,7 +11,21 @@ SENTIMENTS_AS_INDEX = {
 }
 
 class BaseClassifierWithPhoBERT(nn.Module):
-    def __init__(self, hidden_layer):
+    """
+    Base class for classification model with PhoBERT. The whole PhoBERT model is frozen
+        and will not be trained during backpropogation.
+
+    This Module will receive a batch of input_ids and attention mask output from the PhoBERT tokenizer.
+    Then, both are passed into the PhoBERT model to create embeddings, from which the output corresponding
+    to the first token is fed into the Module's `hidden_layer`.
+    """
+    def __init__(self, hidden_layer: nn.Module):
+        """
+        Initialize a `BaseClassifierWithPhoBERT`.
+
+        :param torch.nn.Module hidden_layer: The hidden layer to perform
+            classification on the PhoBERT's embeddings.
+        """
         super().__init__()
         self.phobert = AutoModel.from_pretrained("vinai/phobert-base-v2")
         # Freeze everything
@@ -24,10 +38,28 @@ class BaseClassifierWithPhoBERT(nn.Module):
         return self.hidden_layer(phobert_embeddings['last_hidden_state'][:, 0, :])
     
 class MLPClassifierWithPhoBERT(BaseClassifierWithPhoBERT):
+    """
+    Multi-layer Perceptron model with PhoBERT. The whole PhoBERT model is frozen
+        and will not be trained during backpropogation.
+
+    This Module will receive a batch of input_ids and attention mask output from the PhoBERT tokenizer.
+    Then, both are passed into the PhoBERT model to create embeddings, from which the output corresponding
+        to the first token is fed into the Module's `hidden_layer`, which comprises of a variable number of 
+        fully connected layers and activation function.
+    """
     def __init__(self,
         inner_dims: Iterable[int] | None = None,
         activation_fn: None | nn.Module = None
     ):
+        """
+        Initialize a `MLPClassifierWithPhoBERT`.
+
+        :param Iterable[int] | None inner_dims: The dimension of the fully connected layer.
+            For example: `[256]` will result in one fully connected layer with a dimension of 256.
+            If `None` is given, the embeddings will be connected straight to the output layer.s
+        :param nn.Module | None: The activation function betwen each layer. If `None`, the ReLU
+            funtion is used.
+        """
         if inner_dims is None:
             inner_dims = []
         if activation_fn is None:
